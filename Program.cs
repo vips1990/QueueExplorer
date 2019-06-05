04923d5ce76a4e022c5572245fa01715f6fff750
Vipin Hans <hans.vipin@gmail.com>
2019-06-05 15:49:05 +0100

    Added option to change queue names and ip address from app.config file

--- Program.cs
+++ Program.cs
@@ -6,100 +6,127 @@ using System.Threading.Tasks;
 using System.Messaging;
 using System.Net.Mail;
 using System.Net;
+using System.Configuration;
 
 namespace QueueExplorer
 {
     [Serializable]
     class Program
     {
+        private static MessageQueue sourceQueue, destinationQueue, localQueue;
+        private string machine, machineIp, queueName, queueGUID, queuePath, queuePathIp;
+
+        public Program()
+        {
+            machine = ConfigurationManager.AppSettings["MSMQMachineName"];
+            machineIp = ConfigurationManager.AppSettings["MSMQMachineName_IP"];
+            queueName = ConfigurationManager.AppSettings["QueueName"];
+            queueGUID = ConfigurationManager.AppSettings["QueueID"];
+            queuePath = machine + "\\" + queueName;
+            queuePathIp = machineIp + "\\" + queueName;
+            sourceQueue = new MessageQueue(string.Format("FormatName:DIRECT=TCP:{0}", queuePathIp + ";poison"), false);
+            destinationQueue = new MessageQueue(string.Format("FormatName:DIRECT=TCP:{0}", queuePathIp + ";retry", false));
+            localQueue = new MessageQueue(string.Format("FormatName:DIRECT=TCP:10.1.51.7\\bomireceiver"), false);
+        }
         static void Main(string[] args)
         {
+            // count number of messages in the queue and display message body
+            countAndReadMessages();
 
-            MessageQueue queue = new MessageQueue("BtMSMQ\\bomireceiver;poison");
-            // MessageQueue queue = new MessageQueue("PUBLIC=8A2FF6B3-3305-4CF3-A0E3-6F3A5E8597EE;poison");
-            MessageQueue destinationqueue = new MessageQueue("BtMSMQ\\bomireceiver;retry");
-            bool NoMessage = true;
-            // Populate an array with copies of all the messages in the queue.
+            // move messages between queues or sub queues
+            moveMessages();
+                                 
+        }
 
-            queue.MessageReadPropertyFilter.SetAll();
-            destinationqueue.MessageReadPropertyFilter.SetAll();
-            Message[] msgs = queue.GetAllMessages();
+        static void countAndReadMessages()
+        {
+            
+            sourceQueue.MessageReadPropertyFilter.SetAll();
+            destinationQueue.MessageReadPropertyFilter.SetAll();
+            Message[] msgs = sourceQueue.GetAllMessages();
 
-            // Loop through the messages.
-            foreach (Message msg in msgs)
-            {
-                // Display the label of each message.
-                Console.WriteLine(msg.Label);
-
-                msg.Formatter = new XmlMessageFormatter(new String[] { "System.String,mscorlib" });
-                // String message1 = msg.Body.ToString();
-                Console.WriteLine(msg.GetType());
-                Console.WriteLine("Arrived time: "+ msg.ArrivedTime);
-                Console.WriteLine("Message label is: "+msg.Label);
-
-                NoMessage = false;
-                byte[] data = new byte[2048];
-                msg.BodyStream.Read(data, 0, 2048);
-                string strMessage = ASCIIEncoding.ASCII.GetString(data);
-                Console.WriteLine(strMessage);
-                
-                
-                msg.Priority = System.Messaging.MessagePriority.Normal;
-                msg.Recoverable = true;
-                
-                destinationqueue.DefaultPropertiesToSend.Recoverable = true;
-                destinationqueue.DefaultPropertiesToSend.UseEncryption = true;
-                
-                // destinationqueue.DefaultPropertiesToSend.UseAuthentication = true;
-                // Console.WriteLine(destinationqueue.Transactional);
-
-
-                
-                MessageQueueTransaction myTransaction = new MessageQueueTransaction();
-
-                try
-                {
-                    // Begin a transaction.
-                    myTransaction.Begin();
-                    Console.WriteLine("Inside transaction ");
-                    destinationqueue.Send(msg, myTransaction);
-
-                    myTransaction.Commit();
-
-                }
-
-                catch (System.Exception e)
-                {
-                    myTransaction.Abort();
-                    throw e;
-
-                }
-
-                finally
-                {
-                    // Dispose of the transaction object.
-                    myTransaction.Dispose();
-                        
-                }
-                
-
-                break;
-                        
-
-            }
-
-            if (NoMessage)
+            if (msgs.Length < 1)
             {
                 Console.WriteLine("Message Queue is Empty");
+            }
+
+            else { 
+            // Loop through the messages.
+                foreach (Message msg in msgs)
+                {
+                    // Display the label of each message.
+                    Console.WriteLine(msg.Label);
+
+                    msg.Formatter = new XmlMessageFormatter(new String[] { "System.String,mscorlib" });
+                    // String message1 = msg.Body.ToString();
+                    Console.WriteLine(msg.GetType());
+                    Console.WriteLine("Arrived time: " + msg.ArrivedTime);
+                    Console.WriteLine("Message label is: " + msg.Label);
+
+                    byte[] data = new byte[2048];
+                    msg.BodyStream.Read(data, 0, 2048);
+                    string strMessage = ASCIIEncoding.ASCII.GetString(data);
+                    Console.WriteLine(strMessage);
+                    
+                }
+
             }
 
             Console.WriteLine("Total number of messages in poison queue are-" + msgs.Length);
             Console.ReadKey();
 
-            
         }
 
+        static void moveMessages()
+        {
+
+            try { 
+
+                System.Console.WriteLine("      sourceQueue: " + sourceQueue.GetAllMessages().Count().ToString());
+                System.Console.WriteLine("      destinationQueue: " + destinationQueue.GetAllMessages().Count().ToString());
+                System.Console.WriteLine("      localQueue: " + localQueue.GetAllMessages().Count().ToString());
 
 
+                System.Console.WriteLine(string.Format("Do you want to move {0} msg from  source queue to destination queue?Yes/No \n", sourceQueue.GetAllMessages().Count().ToString()));
+
+                if (System.Console.ReadLine().ToLower() == "yes")
+                {
+                    //var scope = new TransactionScope(TransactionScopeOption.Required);
+
+                    foreach (var message in sourceQueue.GetAllMessages())
+                    {
+                        destinationQueue.Send(message, MessageQueueTransactionType.Single);
+
+                    }
+
+                    //scope.Complete();
+
+                    Console.WriteLine("Messages moved..");
+                    Console.WriteLine("Do you want to purgue the source queue? Yes/No \n");
+
+                    if (System.Console.ReadLine().ToLower() == "yes")
+                    {
+
+                        sourceQueue.Purge();
+                        System.Console.WriteLine("SourceQueue: " + sourceQueue.GetAllMessages().Count().ToString());
+
+                    }
+
+                    System.Console.WriteLine("");
+                    System.Console.WriteLine("      Message after poison purge: ");
+                    System.Console.WriteLine("      SourceQueue: " + sourceQueue.GetAllMessages().Count().ToString());
+                    System.Console.WriteLine("      destinationQueue: " + destinationQueue.GetAllMessages().Count().ToString());
+                    System.Console.WriteLine("      localQueue: " + localQueue.GetAllMessages().Count().ToString());
+
+                }
+
+            }
+
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
+
 }
